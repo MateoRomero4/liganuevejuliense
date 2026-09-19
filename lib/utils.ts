3c2145966803bc4c966c2b037830150237313971
MateoRomero4 <materomero4321@gmail.com>
2026-09-19 10:50:50 -0300

    updates

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function calculateProdePoints(pred_home: number, pred_away: number, real_home: number, real_away: number) {
   if (real_home === null || real_away === null) return 0;
-  if (pred_home === real_home && pred_away === real_away) return 3;
+  if (pred_home === real_home && pred_away === real_away) return 6;
   const pred_diff = pred_home - pred_away;
   const real_diff = real_home - real_away;
-  if ((pred_diff > 0 && real_diff > 0) || (pred_diff < 0 && real_diff < 0) || (pred_diff === 0 && real_diff === 0)) return 1;
+  if ((pred_diff > 0 && real_diff > 0) || (pred_diff < 0 && real_diff < 0) || (pred_diff === 0 && real_diff === 0)) return 3;
   return 0;
 }
